22c85072672c5248181f0a0d6c2526783b596e87
carlosbr <carlosbarraganrey@gmail.com>
2017-10-17 07:40:15 +0000

    server absolute paths

--- server/base64_to_jpg.py
+++ server/base64_to_jpg.py
@@ -7,12 +7,14 @@ import evaluate
 
 def base64_to_jpg(data, style_idx):
 
+	ROOT = "/home/ubuntu/dl/ps_sc/server/"
 	mode = "JPEG"
+	
 	extension = "png" if mode == "PNG" else "jpg"
-	filename = "original/capture."+extension
-	filename_processed = "processed/capture" + "_processed"+"."+extension
-
-	filename_result = "processed/capture."+extension
+	
+	filename = ROOT+"original/capture."+extension
+	filename_processed = ROOT+"processed/capture" + "_processed"+"."+extension
+	filename_result = ROOT+"processed/capture."+extension
 
 	img = Image.open(BytesIO(base64.b64decode(data)))
 	if (mode == "JPEG"): img = img.convert('RGB')
